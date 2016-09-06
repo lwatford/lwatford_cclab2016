9963f3f97b3bdcda7d240b612639f7f439a0634d
lwatford <watfl245@newschool.edu>
2016-09-06 17:07:16 -0400

    revised

--- Day 1 homework _animate state/assets/script.js
+++ Day 1 homework _animate state/assets/script.js
@@ -1,39 +1,49 @@
-//animate one object
-//when page loads cog rotates to the right
-//create a cog that spins to the right
-//create a cog that spins to the left estabblish after the first cog rotates properly
-
-
 document.addEventListener('DOMContentLoaded', function(){
 
 console.log("Ready!");
 
-var looper = setTimeout("rotateAnimation"); //rotate without a pause
+//GOAL
+//animate one object
+//when page loads cog rotates to the right
+//create a star that spins to the right
+//create a left that spins to the left estabblish after the first cog rotates properly
+
+
+var looper; //rotate without a pause
 var degrees = 0;
-var img1 = document.getElementById("img1");
+var speed = 40;
+var elem = "img1";
+var setTimeout = "rotate";
+var saturnright = x;
 
 
+//rotate id of image 1
 
-//established rotate animation. I want the object to rotate 360 degrees
-function rotateAnimation(){
+function rotate(){
+	var elem = document.getElementById;
 	if(navigator.userAgent.match("Chrome")){
-		cogright.style.WebkitTransfrom = "rotate("+ degrees +"deg)";
-	// }else {
-	// 	elem.style.transform = ("rotate("+degrees+"deg)";
-	// }
-	//no need will see in chrome only
+		elem.style.WebkitTransfrom = "rotate("+ degrees +"deg)";
+	}else {
+		elem.style.transform = "rotate("+degrees+"deg)";
+
+	}
 }
 
-looper = setTimeout("rotateAnimation(" + el + " , " + speed + ")" , speed);
+// setTimeout (function() {
+// 	var x = document.getElementById ();
+// }
+looper = setTimeout("rotate(" + elem + " , " + speed + ")" , speed);
 degrees++;
 if(degrees > 359){
 	degrees = 1;
 }
 
 
-document.getElementById("status").innerHTML = "rotate(" + degrees + "deg)";
 
-};
+document.getElementById("container").innerHTML = "rotate(" + degrees + "deg)";
+document.getElementById("img").innerHTML = "rotate(" + degrees + "deg)";
 
+//execute line
 rotateAnimation();
+setTimeout();
 });
