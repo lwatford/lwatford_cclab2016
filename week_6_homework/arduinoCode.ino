16b49ef36afb178bfb2a0025d28bfe5484b82731
lwatford <watfl245@newschool.edu>
2016-10-11 18:25:28 -0400

    week_6_homework

---
+++ week_6_homework/arduinoCode.ino
@@ -0,0 +1,20 @@
+//Specify your input pin here
+int sensePin = 12;
+
+void setup()
+{
+  analogReference(DEFAULT);
+  Serial.begin(9600); 
+}
+
+void loop()
+{
+//Change 300 and 750 to min and max values recieved from
+//Serial port
+   int val = map(analogRead(sensePin), 120, 720, 0, 500);
+   //Now that you figured them out, change
+   //"analogRead(sensePin)" on the next line to
+   //"val"
+  Serial.println(val);
+  delay(100);
+}
