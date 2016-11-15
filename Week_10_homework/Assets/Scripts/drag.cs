d87f2885735d74c8d56a7a086194c2a9a963dbfc
lwatford <watfl245@newschool.edu>
2016-11-15 15:49:17 -0500

    Week_10_homework

---
+++ Week_10_homework/Assets/Scripts/drag.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drag : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+
+	//follow cameras, procedural Animations, its best to use late update
+	//late update runs all items have been processed
+
+
+	void LateUpdate() {
+		transform.position = player.transform.position + offset;
+	}
+}
+
+//homework add timer 
+//make game interesting
