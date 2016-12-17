70d54b5ff5b0f18e001c2ca4354015dc48af8658
lwatford <watfl245@newschool.edu>
2016-12-17 09:02:59 -0500

    Week_15_fianls

---
+++ score.cs
@@ -0,0 +1,34 @@
+﻿//using UnityEngine;
+//using UnityEngine.UI;
+//using System.Collections;
+//
+//public class score : MonoBehaviour {
+//
+//	private int count;
+//
+//	// Use this for initialization
+//	void Start () {
+//		
+//		count = 0;
+////		SetCountText ();
+//	
+//	}
+//	
+//	// Update is called once per frame
+//
+//		void OnTriggerEnter(Collider other) {
+//
+//		if(other.gameObject.CompareTag("Pick Up"))
+//		{
+//			other.gameObject.SetActive(false);
+//			count = count + 1;
+////			SetCountText ();
+//		}
+//	}
+//	void setCountText (){
+//	
+////		countText.text = "Count:" + count.ToString ();
+//	}
+//
+//}
+//
